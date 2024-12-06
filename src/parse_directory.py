aa9947f474477f7d71de1e975b2ed09776641aaa
mwooll <woolleymark01@gmail.com>
2024-12-06 15:35:31 +0100

    added hidden ".keep" files to empty directories in "data/csv/.."

--- src/parse_directory.py
+++ src/parse_directory.py
@@ -26,12 +26,13 @@ def prepare_directories(source_dir):
     write_dirs = [text_dir, csv_dir]
 
     dirs = next(os.walk(source_dir))[1]
-    new_folders = create_subdirectories(write_dirs, dirs)
+    new = create_subdirectories(write_dirs, dirs)
 
-    return files_removed, new_folders
+    return files_removed, new
 
 def create_subdirectories(folders, sub_dirs):
     new_folders = []
+    new_files = []
     for folder in folders:
         for sub in sub_dirs:
             path = folder + sub
@@ -40,9 +41,17 @@ def create_subdirectories(folders, sub_dirs):
             if not os.path.isdir(path):
                 os.mkdir(path)
                 new_folders.append(path)
-    return new_folders
+
+            # create ".keep" file to make git track the empty directory
+            keep_path = path + "/.keep"
+            if not os.path.isfile(keep_path):
+                with open(keep_path, "w+") as file:
+                    file.write("almost empty file to make git track the otherwise empty directory")
+                new_files.append(keep_path)
+                
+    return new_folders, new_files
 
 if __name__ == "__main__":
     source_dir = "data/source/"
-    out = rename_files(source_dir)
+    out = prepare_directories(source_dir)
     print(out)
