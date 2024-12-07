2270d3daa638666f9449b26d1fdb80bf41ad1f8c
mwooll <woolleymark01@gmail.com>
2024-12-07 12:01:17 +0100

    intermediate commit of parse_modules.py

--- src/parse_modules.py
+++ src/parse_modules.py
@@ -3,7 +3,7 @@ import csv
 from pathlib import Path
 
 # possible header for final csv
-csv_header = ["Name", "Number", "Type", "ECTS", "Language", "Responsible Instructor",
+csv_header = ["Name", "Number", "Type", "ECTS", "Language", "Responsible instructor",
               "Prerequisites", "Prior Knowledge", "Component"]
 
 def parse_modules(txt_dir):
@@ -18,8 +18,6 @@ def parse_modules(txt_dir):
                 csv_file = txt_file.replace("text", "csv").replace(".txt", ".csv")
                 # skip files which have already been converted to csv
                 # if not os.path.isfile(csv_file):
-                    # found a file we need to convert to csv
-
                 convert_txt_to_csv(txt_file, csv_file)
                 new_files.append(csv_file)
 
@@ -31,28 +29,38 @@ def convert_txt_to_csv(txt_file, csv_file):
     # a new module always starts with "Page 1", the very first entry is empty
     modules = text.split("Page 1")[1:]
 
-    extract_fields_from_modules(modules[:1])
+    data = extract_fields_from_modules(modules[:1])
 
-    data = {}
     with open(csv_file, mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=["Responsible", "Prerequisites"])
+        writer = csv.DictWriter(file, fieldnames=csv_header)
         writer.writeheader()
         writer.writerows(data)
 
 def extract_fields_from_modules(modules):
     results = []
+
     for module in modules:
-        print(module)
         module_info = {}
+
+        ################
+        # remove later, but speeds up computation while working on it
+        offered = module.find("Offered in:")
+        if offered > 0:
+            module = module[:offered]
+        print(module)
+        ################
+
         lines = module.split("\n")
 
-        for line in lines:
-            if "Responsible instructor:" in line:
-                instructor = line.split("Responsible instructor:")[-1].strip()
-                
-                instructor_name = instructor.split("Requirements")[0].strip()
-                module_info["Responsible Instructor"] = instructor_name
-                break
+        # one liners
+        module_info["Name"] = lines[4][7:]
+
+
+        lines = lines[5:]
+
+        # needs to be ordered as occurring in file
+        simple_fields = ["ECTS", "Responsible instructor"]
+        lines, module_info = find_one_line_fields(lines, simple_fields, module_info)
     
         for line in lines:
             if "Prerequisites:" in line:
@@ -65,11 +73,23 @@ def extract_fields_from_modules(modules):
                 
                 module_info["Prerequisites"] = prerequisites
                 break
-    
+
+        print(module_info)
         if module_info:  
             results.append(module_info)
         
     return results
+
+def find_one_line_fields(lines, keywords, module_info):
+    for word in keywords:
+        length = len(word)
+        for index, line in enumerate(lines):
+            if line[:length] == word:
+                # ignore ": "
+                module_info[word] = line[length+2:]
+                break
+        lines = lines[index:]
+    return lines, module_info
 
 
 if __name__ == "__main__":
