81af955860477f8b65ff696c999f2664f3c62df5
avesanim <marcelphilipp.avesani@uzh.ch>
2024-12-16 10:09:34 +0100

    Update parse_time_schedule.py
    Added event type column

--- src/parse_time_schedule.py
+++ src/parse_time_schedule.py
@@ -5,6 +5,7 @@ def extract_event_data(text):
     # search patterns
     event_name_pattern = r"Event (.+)"
     event_code_pattern = r"([A-Za-z0-9._-]+) /"
+    event_type_pattern = r"[A-Za-z0-9._-]+ / (.+?) /"
     instructor_pattern = r"Instructor: (.+)"
     languages_pattern = r"Languages: (.+)"
     component_of_module_pattern = r"Module: (.+)"
@@ -12,17 +13,19 @@ def extract_event_data(text):
     events = []
     
     # blocks of modules at the beginning of each (page 1)
-    module_blocks = re.split(r"Page 1 of \d+", text)
+    module_blocks = re.split(r"Printing Date:", text)
     
     for block in module_blocks:
         event_name_match = re.search(event_name_pattern, block)
         event_code_match = re.search(event_code_pattern, block)
+        event_type_match = re.search(event_type_pattern, block)
         instructor_match = re.search(instructor_pattern, block)
         languages_match = re.search(languages_pattern, block)
         component_of_module_match = re.search(component_of_module_pattern, block)
         # extract and clean up
         event_name = event_name_match.group(1).strip() if event_name_match else "N/A"
         event_code = event_code_match.group(1).strip() if event_code_match else "N/A"
+        event_type = event_type_match.group(1).strip() if event_type_match else "N/A"
         instructor = instructor_match.group(1).strip() if instructor_match else "N/A"
         languages = languages_match.group(1).strip() if languages_match else "N/A"
         compentent_of_module = component_of_module_match.group(1).strip() if component_of_module_match else "N/A"
@@ -31,6 +34,7 @@ def extract_event_data(text):
         events.append({
             "Event Name": event_name,
             "Event Code": event_code,
+            "Event Type": event_type,
             "Instructor": instructor,
             "Course Languages": languages,
             "Component of Module": compentent_of_module
@@ -47,7 +51,7 @@ def main(input_file, output_file):
     event_data = extract_event_data(text)
     
     df = pd.DataFrame(event_data)
-    df.to_csv(output_file, index=False)
+    df.to_csv(output_file, index=False, header= True)
     print(f"CSV file saved at: {output_file}")
 
 
@@ -59,3 +63,4 @@ for semester in semesters:
     input_file = f"data/text/{semester}/VVZ_{semester}_time_schedules.txt"  
     output_file = f"data/csv/{semester}/VZZ_{semester}_time_schedule.csv"  
     main(input_file, output_file)
+
